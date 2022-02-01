5de0f849d43bfd516553f1a6e7599c797e5da5a2
Sohrab Chegini <sohrab.chegini@gmail.com>
2022-02-01 13:01:02 +0330

    cleaned up backend code

--- server/index.js
+++ server/index.js
@@ -1,109 +1,17 @@
-const request = require("request")
-const cheerio = require("cheerio")
-const fs = require("fs")
-const { performance } = require("perf_hooks")
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+import { getJobs } from "./job-finder.js"
 
-async function getPageHTML(url) {
-  return new Promise((resolve, reject) => {
-    request(url, { jar: true }, (err, res, body) => {
-      if (err) reject(err)
-      resolve(body)
-    })
-  })
-}
+const app = express()
 
-async function getJobs(url) {
-  let jobs = []
-  let html
+app.use(cors())
+app.use(express.json())
 
-  if (url.includes("?")) {
-    html = await getPageHTML(url.concat("&page=1"))
-  } else {
-    html = await getPageHTML(url.concat("?page=1"))
-  }
+app.post("/", async (req, res) => {
+  res.json(await getJobs(req.body.url))
+})
 
-  const jobSectionSelector =
-    "#__next > div.chakra-container.css-bux6p2 > main > section > div.chakra-stack.css-1536cui"
-
-  let $ = cheerio.load(html)
-  let j = 1
-
-  while ($(jobSectionSelector).length > 0) {
-    let numberOfJobs = $(jobSectionSelector)[0].children.length
-
-    for (let i = 1; i <= numberOfJobs; i++) {
-      const description = $(
-        jobSectionSelector.concat(`> article:nth-child(${i})`)
-      )
-        .find("h2")
-        .text()
-
-      const tech = $(jobSectionSelector.concat(`> article:nth-child(${i})`))
-        .find(".css-q64f56.e1pk5grm2")[0]
-        .children.map((div) => div.children[0].data)
-
-      const details = $(jobSectionSelector.concat(`> article:nth-child(${i})`))
-        .find(".chakra-stack.css-l6bi3f")[0]
-        .children.filter((node) => {
-          if (node.name == "span" || node.name == "p") return node
-        })
-        .map((node) => node.children[0].data)
-
-      const company = $(jobSectionSelector.concat(`> article:nth-child(${i})`))
-        .find(".chakra-stack.css-6lg29b")
-        .find(`p`)
-        .text()
-
-      const city = $(jobSectionSelector.concat(`> article:nth-child(${i})`))
-        .find(".chakra-stack.css-5ngv18")
-        .find(`span`)
-        .text()
-
-      const posted = $(jobSectionSelector.concat(`> article:nth-child(${i})`))
-        .find(`.chakra-stack.css-nm8t2j`)
-        .find(`span`)
-        .attr(`title`)
-
-      const link = $(jobSectionSelector.concat(`> article:nth-child(${i})`))
-        .find(`.css-1yp4ln`)
-        .find(`h2 > a`)
-        .attr("href")
-
-      jobs.push({
-        description: description,
-        tech: tech,
-        details: details,
-        company: company,
-        city: city,
-        posted: posted,
-        link: "https://quera.ir".concat(link),
-      })
-    }
-
-    if (url.includes("?")) {
-      html = await getPageHTML(url.concat(`&page=${++j}`))
-    } else {
-      html = await getPageHTML(url.concat(`?page=${++j}`))
-    }
-
-    $ = cheerio.load(html)
-  }
-
-  fs.writeFile("./jobs.json", JSON.stringify(jobs, null, 2), (err) => {
-    if (err) console.error(err)
-  })
-}
-
-;(async () => {
-  let startTime = performance.now()
-
-  await getJobs("https://quera.ir/magnet/jobs")
-
-  let endTime = performance.now()
-
-  console.log(
-    `It took ${Math.round(
-      (endTime - startTime) / 1000
-    )} seconds for jobs to be fetched`
-  )
-})()
+app.listen(process.env.PORT, () =>
+  console.log(`Listening at http://localhost:${process.env.PORT}/`)
+)
---
+++ server/job-finder.js
@@ -0,0 +1,78 @@
+import request from "request"
+import cheerio from "cheerio"
+
+export async function getJobs(url) {
+  let jobs = []
+  let html
+
+  if (url.includes("?")) {
+    html = await getPageHTML(url.concat("&page=1"))
+  } else {
+    html = await getPageHTML(url.concat("?page=1"))
+  }
+
+  let $ = cheerio.load(html)
+  let j = 1
+  const jobSectionSelector =
+    "#__next > div.chakra-container.css-bux6p2 > main > section > div.chakra-stack.css-1536cui "
+  let jobSection = $(jobSectionSelector)
+
+  while (jobSection.length > 0) {
+    let numberOfJobs = jobSection[0].children.length
+
+    for (let i = 1; i <= numberOfJobs; i++) {
+      let job = $(jobSectionSelector.concat(`> article:nth-child(${i})`))
+
+      const description = job.find(".chakra-heading.css-1d6x238").text()
+
+      const requirements = job
+        .find(".css-q64f56.e1pk5grm2")[0]
+        .children.map((node) => node.children[0].data)
+
+      const details = job
+        .find(".chakra-stack.css-l6bi3f")[0]
+        .children.filter((node) => {
+          if (node.name == "span" || node.name == "p") return node
+        })
+        .map((node) => node.children[0].data)
+
+      const company = job.find(".chakra-text.css-1m52y4d").text()
+
+      const city = job.find(".chakra-stack.css-5ngv18").text()
+
+      const posted = job.find(".chakra-stack.css-nm8t2j").find("span").text()
+
+      const link = job.find(".chakra-link.css-f4h6uy").attr("href")
+
+      jobs.push({
+        description: description,
+        requirements: requirements,
+        details: details,
+        company: company,
+        city: city,
+        posted: posted,
+        link: "https://quera.ir".concat(link),
+      })
+    }
+
+    if (url.includes("?")) {
+      html = await getPageHTML(url.concat(`&page=${++j}`))
+    } else {
+      html = await getPageHTML(url.concat(`?page=${++j}`))
+    }
+
+    $ = cheerio.load(html)
+    jobSection = $(jobSectionSelector)
+  }
+
+  return jobs
+}
+
+async function getPageHTML(url) {
+  return new Promise((resolve, reject) => {
+    request(url, { jar: true }, (err, res, body) => {
+      if (err) reject(err)
+      resolve(body)
+    })
+  })
+}
